ab2e7a15c370d6f4a5dca3dffabd2e5aa9773deb
yasmin <yalharasis@pando-soft.com>
2023-03-13 11:12:26 +0300

    sample of use Astro with markdown file

---
+++ src/components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul v-for="link in links" :key="link.id">
+      <li>
+        <a :href="link.page">{{ link.text }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: "About",
+          page: "/about",
+        },
+        {
+          id: 1,
+          text: "page 1",
+          page: "/page1",
+        },
+        {
+          id: 2,
+          text: "page 2",
+          page: "/page2",
+        },
+      ],
+    };
+  },
+};
+</script>
